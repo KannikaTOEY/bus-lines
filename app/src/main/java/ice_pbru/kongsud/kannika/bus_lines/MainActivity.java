25666f0473da9dde6c2647ff269f495110dca7d3
KannikaTOEY <toeymickey@hotmail.com>
2016-04-07 17:11:49 +0700

    test

---
+++ app/src/main/java/ice_pbru/kongsud/kannika/bus_lines/MainActivity.java
@@ -0,0 +1,13 @@
+package ice_pbru.kongsud.kannika.bus_lines;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
